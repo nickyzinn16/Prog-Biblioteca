138b6a8548666a2e732f31d4acf6ea87d77e1648
nickyzinn16 <rickydavidbans27@gmail.com>
2026-04-01 21:52:27 -0100

    Corrigindo erros no Index.html

--- backend/app.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-import os 
-
-app = Flask(__name__, template_folder='../frontend/templates', static_folder='../frontend/static')
-
-app.secret_key = 'progbiblioteca'
-
-from routes import *
